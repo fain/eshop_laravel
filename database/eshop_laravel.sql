ce3bc21f121949b961d5df93ddb56181f4c33a89
Aiman Yuuhi <yuuhi87@gmail.com>
2016-11-04 17:58:21 +0800

    db

--- database/eshop_laravel.sql
+++ database/eshop_laravel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2016 at 10:27 AM
+-- Generation Time: Nov 04, 2016 at 10:57 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -129,11 +129,12 @@ CREATE TABLE `categories` (
   `main_category_id` int(11) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `slug` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `menu_set` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
   `menu_type` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `status` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   `created_at_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `updated_at_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -142,15 +143,17 @@ CREATE TABLE `categories` (
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `main_category_id`, `name`, `slug`, `menu_type`, `status`, `description`, `created_at`, `updated_at`, `created_at_ip`, `updated_at_ip`) VALUES
-(1, 0, 'MENS', 'mens', 'main', 'A', '', NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(2, 0, 'WOMENS', 'womens', 'main', 'A', '', NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(3, 0, 'KIDS', 'kids', 'main', 'A', '', NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(4, 0, 'FASHION', 'fashion', 'main', 'A', '', NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(5, 0, 'CLOTHING', 'clothing', 'main', 'A', '', NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(6, 1, 'Top', 'top', 'sub', 'A', '', NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(7, 1, 'bottom', 'bottom', 'sub', 'A', '', NULL, NULL, '', ''),
-(8, 3, 'cap', 'cap', 'sub', 'A', 'This is all about cap', NULL, '2016-09-29 06:57:53', '', NULL);
+INSERT INTO `categories` (`id`, `main_category_id`, `name`, `slug`, `menu_set`, `menu_type`, `status`, `description`, `created_at`, `updated_at`, `created_at_ip`, `updated_at_ip`) VALUES
+(1, 0, 'MENS', 'mens', 'Y', 'main', 'A', '', NULL, '2016-11-02 20:10:07', '', NULL),
+(2, 0, 'WOMENS', 'womens', 'Y', 'main', 'A', '', NULL, '2016-11-02 20:10:15', '', NULL),
+(3, 0, 'KIDS', 'kids', 'Y', 'main', 'A', '', NULL, '2016-11-02 20:10:44', '', NULL),
+(5, 0, 'CLOTHING', 'clothing', '', 'main', 'A', '', NULL, NULL, '', '0000-00-00 00:00:00'),
+(6, 1, 'Top', 'top', '', 'sub', 'A', '', NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, 1, 'bottom', 'bottom', '', 'sub', 'A', '', NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, 3, 'cap', 'cap', '', 'sub', 'A', 'This is all about cap', NULL, '2016-09-28 22:57:53', '0000-00-00 00:00:00', NULL),
+(9, 0, 'Sport', 'sport', 'Y', 'main', 'A', '', '2016-11-02 17:47:50', '2016-11-02 17:47:50', NULL, NULL),
+(10, 0, 'Electronics', 'electronic', 'Y', 'main', 'A', '', '2016-11-02 17:49:55', '2016-11-02 17:49:55', NULL, NULL),
+(11, 10, 'Mobiles', 'mobiles', NULL, 'sub', 'A', '', '2016-11-02 17:50:45', '2016-11-02 17:50:45', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -269,7 +272,7 @@ INSERT INTO `merchants` (`id`, `username`, `password`, `user_group`, `remember_t
 
 CREATE TABLE `merchants_bus_details` (
   `id` int(11) NOT NULL,
-  `merchants_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(12) NOT NULL,
@@ -281,16 +284,16 @@ CREATE TABLE `merchants_bus_details` (
   `rtn_state` varchar(3) NOT NULL,
   `rtn_city` varchar(100) NOT NULL,
   `rtn_pcode` varchar(5) NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `created_at` datetime NOT NULL
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `merchants_bus_details`
 --
 
-INSERT INTO `merchants_bus_details` (`id`, `merchants_id`, `name`, `email`, `phone`, `ship_add`, `ship_state`, `ship_city`, `ship_pcode`, `rtn_add`, `rtn_state`, `rtn_city`, `rtn_pcode`, `updated_at`, `created_at`) VALUES
-(1, 1, 'Aiman Zaidan', 'aiman@multibase.com.my', '0176219306', 'No 18, Kedai Jkkk, Jalan Besar, Kampong Soeharto', '10', 'Kuala Kubu Bharu', '44010', 'No 18, Kedai Jkkk, Jalan Besar', '10', 'Kuala Kubu Bharu', '44010', '2016-09-21 08:29:16', '2016-09-21 08:29:16');
+INSERT INTO `merchants_bus_details` (`id`, `user_id`, `name`, `email`, `phone`, `ship_add`, `ship_state`, `ship_city`, `ship_pcode`, `rtn_add`, `rtn_state`, `rtn_city`, `rtn_pcode`, `updated_at`, `created_at`) VALUES
+(1, 3, 'Aiman Zaidan', 'aiman@multibase.com.my', '0176219306', 'No 18, Kedai Jkkk, Jalan Besar, Kampong Soeharto', '10', 'Kuala Kubu Bharu', '44010', 'No 18, Kedai Jkkk, Jalan Besar', '10', 'Kuala Kubu Bharu', '44010', '2016-10-27 20:46:28', '2016-10-27 20:46:28');
 
 -- --------------------------------------------------------
 
@@ -300,24 +303,52 @@ INSERT INTO `merchants_bus_details` (`id`, `merchants_id`, `name`, `email`, `pho
 
 CREATE TABLE `merchants_info` (
   `id` int(11) NOT NULL,
-  `merchants_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `dob` date NOT NULL,
-  `phone` varchar(12) NOT NULL,
-  `gender` varchar(2) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `store_name` varchar(30) NOT NULL,
   `store_url` varchar(30) NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `created_at` datetime NOT NULL
+  `status` varchar(20) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `merchants_info`
 --
 
-INSERT INTO `merchants_info` (`id`, `merchants_id`, `name`, `email`, `dob`, `phone`, `gender`, `store_name`, `store_url`, `updated_at`, `created_at`) VALUES
-(1, 1, 'Aiman Zaidan', 'aiman@multibase.com.my', '1987-08-18', '0176219306', 'F', 'AiFa Store', 'aifastore', '2016-09-21 08:29:16', '2016-09-21 08:29:16');
+INSERT INTO `merchants_info` (`id`, `user_id`, `store_name`, `store_url`, `status`, `updated_at`, `created_at`) VALUES
+(1, 3, 'AiFa Store', 'aifastore', 'Approved', '2016-10-28 04:51:23', '2016-10-27 20:46:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merchant_return`
+--
+
+CREATE TABLE `merchant_return` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `postcode` varchar(10) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merchant_shipping`
+--
+
+CREATE TABLE `merchant_shipping` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `postcode` varchar(10) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -410,12 +441,13 @@ INSERT INTO `posts` (`id`, `url`, `title`, `description`, `content`, `image`, `b
 
 CREATE TABLE `products` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `price` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(140) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
   `category_id` int(11) NOT NULL,
   `brand_id` int(11) NOT NULL,
+  `merchants_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at_ip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -426,10 +458,125 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `title`, `description`, `price`, `category_id`, `brand_id`, `created_at`, `updated_at`, `created_at_ip`, `updated_at_ip`) VALUES
-(1, 'Mini skirt black edition', 'Mini skirt black edition', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', 35, 1, 1, NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(2, 'T-shirt blue edition', 'T-shirt blue edition', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', 64, 2, 3, NULL, NULL, '192.168.0.77', '192.168.0.77'),
-(3, 'Sleeveless Colorblock Scuba', 'Sleeveless Colorblock Scuba', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', 13, 3, 2, NULL, NULL, '192.168.0.77', '192.168.0.77');
+INSERT INTO `products` (`id`, `name`, `title`, `description`, `price`, `category_id`, `brand_id`, `merchants_id`, `created_at`, `updated_at`, `created_at_ip`, `updated_at_ip`) VALUES
+(1, 'Mini skirt black edition', 'Mini skirt black edition', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', 35, 1, 1, 0, NULL, NULL, '192.168.0.77', '192.168.0.77'),
+(2, 'T-shirt blue edition', 'T-shirt blue edition', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', 64, 2, 3, 0, NULL, NULL, '192.168.0.77', '192.168.0.77'),
+(3, 'Sleeveless Colorblock Scuba', 'Sleeveless Colorblock Scuba', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', 13, 3, 2, 0, NULL, NULL, '192.168.0.77', '192.168.0.77');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products_info`
+--
+
+CREATE TABLE `products_info` (
+  `id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `prod_name` varchar(255) NOT NULL,
+  `prod_code` varchar(50) NOT NULL,
+  `prod_img_id` int(11) DEFAULT NULL,
+  `details` text NOT NULL,
+  `sales_type` varchar(10) NOT NULL,
+  `selling_period_set` varchar(1) NOT NULL,
+  `selling_period_day` varchar(15) DEFAULT NULL,
+  `selling_period_start` date DEFAULT NULL,
+  `selling_period_end` date DEFAULT NULL,
+  `price` double NOT NULL,
+  `tier_price` varchar(15) DEFAULT NULL,
+  `inst_price` double DEFAULT NULL,
+  `discount_set` varchar(1) DEFAULT NULL,
+  `discount_sel` varchar(15) DEFAULT NULL,
+  `discount_val` double DEFAULT NULL,
+  `discount_period_set` varchar(1) DEFAULT NULL,
+  `discount_period_start` date DEFAULT NULL,
+  `discount_period_end` date DEFAULT NULL,
+  `weight` double DEFAULT NULL,
+  `stock_quantity` int(11) DEFAULT NULL,
+  `option_id` int(11) DEFAULT NULL,
+  `merchant_shipping_id` int(11) NOT NULL,
+  `merchant_return_id` int(11) NOT NULL,
+  `shipping_method` varchar(20) NOT NULL,
+  `ship_rate` varchar(15) NOT NULL,
+  `ship_rate_id` int(11) DEFAULT NULL,
+  `after_sale_serv` text,
+  `return_policy` text NOT NULL,
+  `promo_set` varchar(1) DEFAULT NULL,
+  `promo_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_image`
+--
+
+CREATE TABLE `product_image` (
+  `id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promotions`
+--
+
+CREATE TABLE `promotions` (
+  `id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
+  `promo_text` varchar(255) NOT NULL,
+  `country_origin` varchar(100) NOT NULL,
+  `mul_pur_disc_set` varchar(1) DEFAULT NULL,
+  `mul_pur_disc_item` int(11) DEFAULT NULL,
+  `mul_pur_disc_sel` varchar(10) DEFAULT NULL,
+  `mul_pur_disc` double DEFAULT NULL,
+  `mul_pur_disc_period_set` varchar(1) DEFAULT NULL,
+  `mul_pur_period_start` date DEFAULT NULL,
+  `mul_pur_period_end` date DEFAULT NULL,
+  `min_pur` varchar(20) DEFAULT NULL,
+  `min_pur_val` int(11) DEFAULT NULL,
+  `max_pur` varchar(20) DEFAULT NULL,
+  `max_per_ord` int(11) DEFAULT NULL,
+  `max_per_pers` int(11) DEFAULT NULL,
+  `ad_sel` varchar(10) NOT NULL,
+  `ad_start` date DEFAULT NULL,
+  `ad_end` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping_rate`
+--
+
+CREATE TABLE `shipping_rate` (
+  `id` int(11) NOT NULL,
+  `wm_kg` float DEFAULT NULL,
+  `wm_rm` double DEFAULT NULL,
+  `same_all_reg` varchar(1) DEFAULT NULL,
+  `add_wm_kg` float DEFAULT NULL,
+  `add_wm_rm` double DEFAULT NULL,
+  `sbh_kg` float DEFAULT NULL,
+  `sbh_rm` double DEFAULT NULL,
+  `add_sbh_kg` float DEFAULT NULL,
+  `add_sbh_rm` double DEFAULT NULL,
+  `srk_kg` float DEFAULT NULL,
+  `srk_rm` double DEFAULT NULL,
+  `add_srk_kg` float DEFAULT NULL,
+  `add_srk_rm` double DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -476,10 +623,43 @@ CREATE TABLE `users` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user_group` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_group`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'Zaidan Hussein', 'zaidan60@gmail.com', '$2y$10$9OKPHyBjQ43yF2HdM.3D0.GQNWNi1eNUjDZ018Xaia7/12SAZ8qJW', 'Customer', 'PImi0Jpza4LAMMtWCEbWcE6LaTNO6tPQs4i8MOjVGyq8ULrajamhRoe0HKKo', '2016-10-26 19:57:54', '2016-10-27 00:03:59'),
+(3, 'Aiman Zaidan', 'aiman@multibase.com.my', '$2y$10$YcAwNhmlPa7DcBZTNqEbVueCpYg5TN4Tfz7JtMkw5zhjzHbSOfi.G', 'Admin', 'Rmx91BIDGuk6ClI2FGxLDCzFtPTjBKr8XEpB68b8voeoos1nFzFesUZn9YvH', '2016-10-27 20:46:28', '2016-10-28 00:08:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(2) NOT NULL,
+  `c_number` varchar(12) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`id`, `user_id`, `dob`, `gender`, `c_number`, `created_at`, `updated_at`) VALUES
+(1, 2, '1960-01-19', 'F', '0129811617', '2016-10-26 19:57:54', '2016-10-26 19:57:54'),
+(2, 3, '1987-08-18', 'F', '0176219306', '2016-10-27 20:46:28', '2016-10-27 20:46:28');
 
 --
 -- Indexes for dumped tables
@@ -544,6 +724,18 @@ ALTER TABLE `merchants_info`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `merchant_return`
+--
+ALTER TABLE `merchant_return`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `merchant_shipping`
+--
+ALTER TABLE `merchant_shipping`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -565,6 +757,30 @@ ALTER TABLE `products`
   ADD UNIQUE KEY `products_name_unique` (`name`);
 
 --
+-- Indexes for table `products_info`
+--
+ALTER TABLE `products_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_image`
+--
+ALTER TABLE `product_image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `promotions`
+--
+ALTER TABLE `promotions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shipping_rate`
+--
+ALTER TABLE `shipping_rate`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `state`
 --
 ALTER TABLE `state`
@@ -576,6 +792,12 @@ ALTER TABLE `state`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -605,7 +827,7 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `city`
 --
@@ -627,6 +849,16 @@ ALTER TABLE `merchants_bus_details`
 ALTER TABLE `merchants_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `merchant_return`
+--
+ALTER TABLE `merchant_return`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `merchant_shipping`
+--
+ALTER TABLE `merchant_shipping`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
@@ -637,6 +869,26 @@ ALTER TABLE `posts`
 ALTER TABLE `products`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `products_info`
+--
+ALTER TABLE `products_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_image`
+--
+ALTER TABLE `product_image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `promotions`
+--
+ALTER TABLE `promotions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `shipping_rate`
+--
+ALTER TABLE `shipping_rate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `state`
 --
 ALTER TABLE `state`
@@ -645,7 +897,12 @@ ALTER TABLE `state`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
