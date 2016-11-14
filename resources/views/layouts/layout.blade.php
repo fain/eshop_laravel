c0f4123c48b2663f2af2cf200815bf651d66e9b6
fain <fainislamic@gmail.com>
2016-11-14 10:47:51 +0800

    Footer
    Change copyright & company url address at footer

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -282,8 +282,8 @@
 		<div class="footer-bottom">
 			<div class="container">
 				<div class="row">
-					<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
-					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+					<p class="pull-left">Copyright © 2016 ANGKASA E-SHOP. All rights reserved.</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.multibase.com.my">Multibase</a></span></p>
 				</div>
 			</div>
 		</div>
