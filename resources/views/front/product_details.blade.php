7eee68d8bf2ffa904bab325dac680ac1317201e7
fain <fainislamic@gmail.com>
2016-11-15 12:27:28 +0800

    Product details
    Basic frame & content layout for product details

---
+++ resources/views/front/product_details.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layout')
+
+@section('content')
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="left-sidebar">
+					@include('shared.sidebar')
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+@endsection
