9769c1d102b4f7971245ea82a4ae86b524ee47fc
fain <fainislamic@gmail.com>
2016-11-18 17:58:56 +0800

    Product tabs
    Layout for product details

--- resources/views/front/product_details.blade.php
+++ resources/views/front/product_details.blade.php
@@ -83,71 +83,362 @@
 						</div>
 					</div><!--/product-details-->
 
-					<div class="category-tab shop-details-tab"><!--category-tab-->
-						<div class="col-sm-12">
-							<ul class="nav nav-tabs">
-								<li class="active"><a href="#details" data-toggle="tab">Product Details</a></li>
-								<li><a href="#reviewscomments" data-toggle="tab">Reviews/Comments</a></li>
-								<li><a href="#qa" data-toggle="tab">Product Q & A</a></li>
-								<li><a href="#sellreturnexchange" data-toggle="tab">Sell/Return/Exchange</a></li>
-							</ul>
-						</div>
+					<div class="row"><!--category-tab-->
+					    	<div class="col-sm-12">
+					            <div class="panel with-nav-tabs panel-default">
+					                <div class="panel-heading">
+					                        <ul class="nav nav-tabs">
+					                            <li class="active"><a href="#details" data-toggle="tab">Details</a></li>
+					                            <li><a href="#reviews_comments" data-toggle="tab">Reviews(0)/Comments(0)</a></li>
+					                            <li><a href="#qa" data-toggle="tab">Product Q & A</a></li>
+					                            <li><a href="#sellerreturnexchange" data-toggle="tab">Seller/Return/Exchange</a></li>
+					                        </ul>
+					                </div>
+					                <div class="panel-body">
+					                    <div class="tab-content">
+					                        <div class="tab-pane fade in active" id="details">
+												<table class="table table-no-hover">
+																<h4>Basic Information of a Product</h4>
+															    <tbody>
+															        <tr>
+															            <th scope class="">Product Status/Sales Type</th>
+															            <td class="">{{$product->stock_quantity}}</td>
+															            <th scope class="">Product No.</th>
+															            <td class="">{{$product->prod_code}}</td> 
+															        </tr>
+															         <tr>
+																		<th scope class="">Brand</th>
+															            <td class="">{{$product->name}}</td>    
+																		<th scope class="">Country of Origin</th>
+															            <td class="">{{$product->name}}</td>          
+															      	</tr>
+															        <tr>
+																		<th scope class="">Weight(kg)</th>
+															            <td colspan=3 class="">{{$product->weight}} kg</td>         
+															      	</tr>
+															      	<tr>
+																		<th scope class="">Shipping Method</th>
+															            <td colspan=3class="">{{$product->shipping_method}}</td>          
+															      	</tr>
+															      		<tr>
+																		<th scope class="">GST Applicable</th>
+															            <td colspan=3class="">{{$product->name}}</td>          
+															      	</tr>
+															      		<tr>
+																		<th scope class="">Tax Invoice</th>
+															            <td colspan=3 class="">{{$product->name}}</td>          
+															      	</tr>
+															      		<tr>
+																		<th scope class="">After Sale Service</th>
+															            <td colspan=3 class="">{{$product->after_sale_serv}}</td>          
+															      	</tr>
+															    </tbody>
+												</table> 
+					                        </div>
 
-						<div class="panel-body">
-		                    <div class="tab-content">
-		                        <div class="tab-pane fade in active" id="details">
-								<div class="col-lg-12">
-									<table class="table table-no-hover">
-										<h4>Basic Information of a Product</h4>
-									    <tbody>
-									        <tr>
-									            <th scope class="">Product Status/Sales Type</th>
-									            <td class="">{{$product->stock_quantity}}</td>
-									            <th scope class="">Product No.</th>
-									            <td class="">{{$product->prod_code}}</td> 
-									        </tr>
-									         <tr>
-												<th scope class="">Brand</th>
-									            <td class="">{{$product->name}}</td>    
-												<th scope class="">Country of Origin</th>
-									            <td class="">{{$product->name}}</td>          
-									      	</tr>
-									        <tr>
-												<th scope class="">Weight(kg)</th>
-									            <td colspan=3 class="">{{$product->weight}} kg</td>         
-									      	</tr>
-									      	<tr>
-												<th scope class="">Shipping Method</th>
-									            <td colspan=3class="">{{$product->shipping_method}}</td>          
-									      	</tr>
-									      		<tr>
-												<th scope class="">GST Applicable</th>
-									            <td colspan=3class="">{{$product->name}}</td>          
-									      	</tr>
-									      		<tr>
-												<th scope class="">Tax Invoice</th>
-									            <td colspan=3 class="">{{$product->name}}</td>          
-									      	</tr>
-									      		<tr>
-												<th scope class="">After Sale Service</th>
-									            <td colspan=3 class="">{{$product->after_sale_serv}}</td>          
-									      	</tr>
-									    </tbody>
-									</table> 
+					                        <div class="tab-pane fade" id="reviews_comments">
+ 
+ 
+      											<div class="table-responsive">
+													<table class="table table-striped">
+														<h4>Customer Reviews</h4>
+														 
+														 <div class="dropdown"  align="right">
+														    <button id="review" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">All Reviews
+														    <span class="caret"></span></button>
+															<ul class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="review">
+														      <li><a href="#">General Review</a></li>
+														      <li><a href="#">Photo Review</a></li>
+														    </ul>
 
-								</div>
+														    <button id="recommendation" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Recommendation
+														    <span class="caret"></span></button>
+															<ul class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="recommendation">
+														      <li><a href="#">Highly Recommended</a></li>
+														      <li><a href="#">Recommended</a></li>
+														      <li><a href="#">Moderately Recommended</a></li>
+														      <li><a href="#">Not Recommended</a></li>
+														    </ul>
+														
+														    <button id="author" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Author Grade
+														    <span class="caret"></span></button>
+															<ul class="dropdown-menu" style="right: 0; left: auto;">
+														      <li><a href="#">Angkasa Staff</a></li>
+														      <li><a href="#">Guest</a></li>
+														    </ul>
+														</div>
 
-		                        </div>
-		                        <div class="tab-pane fade" id="reviewscomments">review comment</div>
-		                        <div class="tab-pane fade" id="qa">q and a</div>
-		                        <div class="tab-pane fade" id="sellreturnexchange">sell</div>
-		                    </div>
-		                </div>
-					</div><!--/category-tab-->
-				<ul>
-					<li>Angkasa E-Shop receives report on products to protect buyers’ right. For further information please contact us. Reportgo</li>
-					<li>For order related and other issues, kindly click on E-mail Enquiry here.  E-mailEnquiry</li>
-				</ul>
+													    <thead> 
+													    	<p>
+															<tr>
+													            <th scope class="">Rating</th>
+													            <th scope class="">Content</th>
+													            <th scope class="">View/Comments</th>
+																<th scope class="">Author/Date</th>
+													        </tr>
+													    </thead>
+													    <tbody id="reviews">
+													         <tr>
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>    
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>          
+													      	</tr>
+													    </tbody>
+													</table> 
+
+													<div class="col-md-12 text-center">
+											    	  <ul class="pagination pagination-lg pager" id="myReviewsPager"></ul>
+											    	</div>
+
+													<table class="table table-striped">
+														<h4>Customer Comments</h4>
+
+														<div class="dropdown"  align="right">
+														    <button id="satisfaction_rating" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Satisfaction Rating
+														    <span class="caret"></span></button>
+															<ul class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="satisfaction_rating">
+														      <li><a href="#">Satisfied</a></li>
+														      <li><a href="#">Average</a></li>
+														      <li><a href="#">Dissatisfied</a></li>
+														    </ul>
+
+														    <button id="recommendation" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Author Grade
+														    <span class="caret"></span></button>
+															<ul class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="menu2">
+														      <li><a href="#">Angkasa Staff</a></li>
+														      <li><a href="#">Guest</a></li>
+														    </ul>
+														</div>
+
+													    <thead> 
+													    	<p>
+															<tr>
+													            <th scope class="">Rating</th>
+													            <th scope class="">Content</th>
+																<th scope class="">Author/Date</th>
+													        </tr>
+													    </thead>
+													    <tbody id="comments">
+													         <tr>
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>          
+													      	</tr>
+													    </tbody>
+													</table> 
+											</div>
+												<div class="col-md-12 text-center">
+										    	  <ul class="pagination pagination-lg pager" id="myCommentsPager"></ul>
+										    	</div>
+					                        </div>
+
+					                        <script type="text/javascript">	
+											$(document).ready(function(){
+											    
+											  $('#comments').pageMe({pagerSelector:'#myCommentsPager',showPrevNext:true,hidePageNumbers:false,perPage:1});
+											    
+											});
+											</script>
+
+					                        <div class="tab-pane fade" id="qa">
+					                        	<div class="table-responsive">
+													<table class="table table-striped">
+														<h4>Product Q & A</h4>
+														 
+														 <div class="dropdown"  align="right">
+														    <button id="topic" class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Topic
+														    <span class="caret"></span></button>
+															<ul class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="topic">
+														      <li><a href="#">Product</a></li>
+														      <li><a href="#">Shipping</a></li>
+   														      <li><a href="#">Return/Cancellation</a></li>
+														      <li><a href="#">Exchange</a></li>
+														      <li><a href="#">Other Topics</a></li>
+														    </ul>
+														</div>
+
+													    <thead> 
+													    	<p>
+															<tr>
+													            <th scope class="">No</th>
+													            <th scope class="">Topic</th>
+													            <th scope class="">Q & A</th>
+																<th scope class="">Author/Date</th>
+													        </tr>
+													    </thead>
+													    <tbody id="qanda">
+													         <tr>
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>    
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>          
+													      	</tr>
+													    </tbody>
+													</table> 
+
+													<div class="col-md-12 text-center">
+											    	  <ul class="pagination pagination-lg pager" id="myQ&APager"></ul>
+											    	</div>
+													
+
+							                        <script type="text/javascript">	
+													$(document).ready(function(){
+													    
+													  $('#comments').pageMe({pagerSelector:'#myQ&APager',showPrevNext:true,hidePageNumbers:false,perPage:1});
+													    
+													});
+													</script>
+
+					                        	</div>
+					                        </div>
+
+					                        <div class="tab-pane fade" id="sellerreturnexchange">
+												<table class="table table-no-hover">
+													<h4>Seller Information</h4>
+												    <tbody>
+												        <tr>
+												            <th scope class="">Seller</th>
+												            <td class="">{{$product->stock_quantity}}</td>
+												        </tr>
+												         <tr>
+															<th scope class="">Ship-From Address</th>
+												            <td class="">{{$product->name}}</td>           
+												      	</tr>
+												        <tr>
+															<th scope class="">Return/Exchange Address</th>
+												            <td colspan=3 class="">{{$product->weight}} kg</td>         
+												      	</tr>
+												      	<tr>
+															<th scope class="">Notice on Return/Exchange</th>
+												            <td colspan=3class="">{{$product->shipping_method}}</td>          
+												      	</tr>
+												    </tbody>
+												</table> 
+
+												<!-- <table class="table table-no-hover">
+													<h4>Return / Exchange Policy</h4>
+													 <thead> 
+													    	<p>
+															<tr>
+													            <th scope class="">Cancellation</th>
+													            <th scope class="">Return</th>
+																<th scope class="">Exchange</th>
+													        </tr>
+													    </thead>
+													    <tbody id="comments">
+													         <tr>
+													            <td class="">
+													            <ul>
+													            	<li>saf</li>
+													            	<li>fsfs</li>
+													            </ul>
+													            </td>          
+													            <td class="">{{$product->name}}</td>          
+													            <td class="">{{$product->name}}</td>          
+													      	</tr>
+													    </tbody>
+
+												</table> -->
+
+												<h4>Return / Exchange Policy</h4>
+
+												<div class = "list-group">
+												   <div class = "list-group-item active">
+												      <h4 class = "list-group-item-heading">
+												         Cancellation
+												      </h4>
+												   </div>
+												   
+												   <div class = "list-group-item">
+												      <h5 class = "list-group-item-heading">Request for cancellation if product has not been dispatched after payment completion.</h5>
+												   </div>
+
+												   <div class="list-group-item">
+												      <h5 class = "list-group-item-heading">
+													  How to Request for Cancellation/Return/Exchange?</h5>
+												      <p class = "list-group-item-text">
+														<div class="well">
+														<ul>
+															<li>You can request for cancellation after completing payment and if product has not been dispatched by then, cancellation request is approved and you can receive refund.
+															But, in case seller has already dispatched product, cancellation request can be rejected.</li>
+															<li>You can request for return after obtaining product, you should send the product in question to seller.
+															After seller checks if returned product is flawed, and approves your request, return is completed and payment is refunded.</li>
+															<li>You can request for exchange if wrong product is shipped, you should send the product in question to seller.
+															After seller checks if returned product is flawed, seller approves your request, and re-sends new product.</li>											      
+														</p>
+												  		</ul>
+												  		</div>
+												   </div>
+												   
+												   
+												   
+												</div>
+
+												<div class = "list-group">
+												   <div class = "list-group-item active">
+												      <h4 class = "list-group-item-heading">
+												         Return
+												      </h4>
+												   </div>
+												   
+												   <div class="list-group-item">
+												      <h5 class = "list-group-item-heading">
+												         Return Policy
+												      </h5>
+												      
+												      <p class = "list-group-item-text">
+														<div class="well">
+														<ul>
+															<li>Request for return within 7 calendar days following delivery date.</li>
+															<li>Check seller’s approval on buyer’s request.</li>
+															<li>You can request for exchange if wrong product is shipped, you should send the product in question to seller.</li>
+															<li>Buyer sends product in question to seller.</li>
+															<li>Seller receives and checks returned product</li>
+															<li>Seller approves return and return is completed.</li>
+														</p>
+												  		</ul>
+												  		</div>
+												   </div>
+												   
+												</div>
+												   
+												<div class = "list-group">
+												   <div class = "list-group-item active">
+												      <h4 class = "list-group-item-heading">
+												         Exchange
+												      </h4>
+												   </div>
+												   
+												   <div class="list-group-item">
+												      <h4 class = "list-group-item-heading">
+												     fsdfafsa
+												      </h4>
+												      
+												      <p class = "list-group-item-text">
+												         Exchange 
+												     </p>
+												   </div>
+												   
+												   <div class = "list-group-item">
+												      <h4 class = "list-group-item-heading">24*7 support</h4>
+												      <p class = "list-group-item-text">We provide 24*7 support.</p>
+												   </div>
+												   
+												</div>
+												</div>
+					                        </div>
+					                    </div>
+					                </div>
+					            </div>
+					        </div>
+					      </div><!--category tab-->
+
+
+					<ul>
+						<li>**Angkasa E-Shop receives report on products to protect buyers’ right. For further information please contact us. Reportgo</li>
+						<li>**For order related and other issues, kindly click on E-mail Enquiry here.  E-mailEnquiry</li>
+					</ul>
 			</div>
 	</div>
 </div>
