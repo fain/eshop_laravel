142463d3cce3dd81762ef69af843799ebd0fd307
fain <fainislamic@gmail.com>
2017-01-13 15:54:55 +0800

    lightbox, top bar menu, slider

--- resources/views/front/login.blade.php
+++ resources/views/front/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout')
 
-@section('content')       
+@section('content')  
+     
        <section id="form"><!--form-->
             <div class="container">
                 <div class="row">
